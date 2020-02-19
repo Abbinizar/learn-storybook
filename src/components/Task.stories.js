9ef535ab063304a82f29114f054e6180fb39928e
Abbi Nizar <abbinizarm@gmail.com>
2020-02-19 18:32:50 +0700

    add test

--- src/components/Task.stories.js
+++ src/components/Task.stories.js
@@ -1,33 +1,35 @@
-import React from "react";
-import { action, actions } from "@storybook/addon-actions";
-import Task from "./Task";
+import React from 'react';
+import { action } from '@storybook/addon-actions';
 
-export default{
-    component: Task,
-    title: 'Task',
-    excludeStories: /.*Data$/,
+import Task from './Task';
+
+export default {
+  component: Task,
+  title: 'Task',
+  // Our exports that end in "Data" are not stories.
+  excludeStories: /.*Data$/,
 };
 
 export const taskData = {
-    id: '1',
-    title: 'Test Task',
-    state: 'TASK_INBOX',
-    updatedAt: new Date(2018, 0, 1, 9, 0)
+  id: '1',
+  title: 'Test Task',
+  state: 'TASK_INBOX',
+  updatedAt: new Date(2018, 0, 1, 9, 0),
 };
 
 export const actionsData = {
-    onPinTask: actions('onPinTask'),
-    onArchieveTask: actions('onArchieveTask')
+  onPinTask: action('onPinTask'),
+  onArchiveTask: action('onArchiveTask'),
 };
 
 export const Default = () => {
-    return <Task task = {{...taskData}} {...actionsData} />
+  return <Task task={{ ...taskData }} {...actionsData} />;
 };
 
-export const Pinned = () => {
-    return <Task task = {{...taskData, state: 'TASK_PINNED' }} {...actionsData} />
-};
+export const Pinned = () => (
+  <Task task={{ ...taskData, state: 'TASK_PINNED' }} {...actionsData} />
+);
 
-export const Archieved = () => {
-    return <Task task = {{...taskData, state: 'TASK_ARCHIEVED'}} {...actionsData} />
-};
+export const Archived = () => (
+  <Task task={{ ...taskData, state: 'TASK_ARCHIVED' }} {...actionsData} />
+);
